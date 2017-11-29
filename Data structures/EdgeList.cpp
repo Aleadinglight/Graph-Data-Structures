322e07091a4df8160d4d4f848c148ea66372ecb6
Aleadinglight <huutam.ptnk@gmail.com>
2017-11-29 03:28:03 +0200

    Create EdgeList.cpp

---
+++ Data structures/EdgeList.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int main()
+{
+    int n,m;
+    // Input n - number of vertices,and m - number of edges
+    cin>>n>>m;
+    priority_queue< pair<int, ii> > EdgeList;  //For edges 1..m
+    // m next lines each contains 3 int "u v c" meaning: edges from u->v with cost c
+    int u,v,c;
+    for (int i=0; i<m; i++){
+        cin>>u>>v>>c;
+
+        EdgeList.push(make_pair(c, make_pair(u, v)));
+    }
+    // Print out the edges stored in Adjacency List
+
+    while (EdgeList.size()>0){
+        cout<<EdgeList.top().second.first<<"->"<<EdgeList.top().second.second<<" costs "<<EdgeList.top().first<<endl;
+        EdgeList.pop();
+    }
+    return 0;
+}
